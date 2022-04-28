277fce407b92afc800572336e5bde252579f26af
nicobelen <nicolasbelen93@gmail.com>
2022-04-28 14:32:35 -0700

    variable load

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,32 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
+def listToString(s):
+    str1 = " "
+
+    return (str1.join(s))
+
 if __name__ == "__main__":
     import hidden_4
 
-for i in dir([hidden_4]):
-    if i.startswith("_", 0, 1) is False:
+hid = dir(hidden_4)
+str1 = listToString(hid)
+str2 = str1.split("\'")
+for str2 in str1:
+    if str2[0:2] != "__":
+        print(str2, end="")
+"""
+if __name__ == "__main__":
+    import hidden_4
+
+def listToString(s):
+    str1 = " "
+
+    return (str1.join(s))
+
+filename = dir(hidden_4)
+str1 = listToString(filename)
+
+for i in str1:
+    hid = str1.split("\'")
+    if str1[i] != "__":
         print(i)
+        """
