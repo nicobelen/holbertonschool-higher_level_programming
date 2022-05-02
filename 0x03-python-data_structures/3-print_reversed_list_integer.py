b9cb1943d6151c045aadd1acd73c43da9d86233d
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 09:15:51 -0700

    remove - 1 to lenlist fro 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    lenlist = len(my_list)
-
+    listlen = len(my_list)
     my_list.reverse()
 
-    for i in range(lenlist):
-        print(my_list[i])
+    for i in range(listlen):
+        print("{}".format(my_list[i]))
