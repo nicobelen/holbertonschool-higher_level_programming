23b0e99ec60f8f3e05e40244c7522d31e017f87c
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 08:50:35 -0700

    test 1-element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     lenlist = len(my_list)
 
-    if idx < 0:
-        return None
-    elif idx > lenlist:
+    if idx < 0 or idx > lenlist:
         return None
     else:
         return (my_list[idx])
