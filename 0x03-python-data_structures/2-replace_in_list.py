9dec8d743cef85f795001622e55e93a7b559fb48
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 09:07:40 -0700

    add - 1 to lenlist

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    lenlist = len(my_list)
+    lenlist = len(my_list) - 1
 
     if idx < 0:
         return my_list
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     newlist = my_list.copy()
 
-    lenlist = len(newlist)
+    lenlist = len(newlist) - 1
 
     if idx < 0:
         return newlist
