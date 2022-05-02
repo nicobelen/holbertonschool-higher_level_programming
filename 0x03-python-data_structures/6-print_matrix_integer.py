7daec6aa95054cf304a25b5d331fe361fe125e6d
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 12:54:37 -0700

    test 6 print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    listlen = len(matrix)
+
+    for i in range(listlen):
+        for j in range(listlen):
+            if j != 0:
+                print(" ", end="")
+            print(matrix[i][j], end="")
+        print("")
