49b4197185985c7b7d9264dd7c17883367391892
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 19:48:55 -0700

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    zipped = zip(tuple_a, tuple_b)
-    mapped = map(sum, zipped)
-    sum1 = tuple(mapped)
+    list1 = [0, 0, 0, 0]
+    list2 = [0, 0, 0, 0]
+    list3 = []
 
-    if len(sum1) < 2:
-        for i in range(len(sum1)):
-            if sum1[i] == None:
-                sum1[i] = 0
-        return sum1
-    elif len(sum1) > 2:
-        return sum1[1, 2]
-    return sum1
+    for i in range(len(tuple_a)):
+        list1[i] = tuple_a[i]
+    for j in range(len(tuple_b)):
+        list2[j] = tuple_b[j]
+    list3 = (list1[0] + list2[0], list1[1] + list2[1])
+    return tuple(list3)
