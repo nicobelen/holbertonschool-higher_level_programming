27d6e38b925511992dd0a04040840835e9a070d1
nicobelen <nicolasbelen93@gmail.com>
2022-05-02 19:57:55 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     count = 0
-    if len(sentence) == 0:
+    if len(sentence) - 1 == 0:
         sentence[0] = None
     for i in sentence:
         count += 1
