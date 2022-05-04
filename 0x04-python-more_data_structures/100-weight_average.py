0d1a8eaa609020886e221deb0989d336a24a0e37
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 15:10:24 -0700

    weight average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    
+    num1 = 0
+    num2 = 0
+
+    if my_list:
+        for i in my_list:
+            num1= num1 + (i[0] * i[1])
+            num2 = num2 + (i[1])
+        return num1 / num2
+    else:
+        return 0
