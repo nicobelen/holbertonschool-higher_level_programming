39938ba4df1663d54ba98434d7ca456ddc9bfb7e
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 11:28:12 -0700

    print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    print("Number: ", a_dictionary['Number'])
+    print("ids: ", a_dictionary['ids'])
+    print("language: ", a_dictionary['language'])
+    print("track: ", a_dictionary['track'])
