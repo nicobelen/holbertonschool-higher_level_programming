688cfd56d0104bc67b54fab9515fd6006b93f5e8
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 10:47:13 -0700

    uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def get_unique_numbers(numbers):
+
+    list_of_unique_numbers = []
+
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+def uniq_add(my_list=[]):
+    numbers = get_unique_numbers(my_list)
+    res = 0
+    
+    for n in numbers:
+        res = res + n
+    return res
