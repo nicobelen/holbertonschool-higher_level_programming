e1422f8ed9369a64513df2a1332f896a09d99940
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 10:32:28 -0700

    search replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    lenlist = len(my_list)
+
+    for i in range(lenlist):
+        if my_list[i] == search:
+            new_list.insert(i, replace)
+    return new_list
