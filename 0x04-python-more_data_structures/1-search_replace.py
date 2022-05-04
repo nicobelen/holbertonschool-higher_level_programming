2bb56378950b6a75502a281452aff38d31b9d3d8
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 10:37:24 -0700

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,6 @@ def search_replace(my_list, search, replace):
 
     for i in range(lenlist):
         if my_list[i] == search:
+            new_list.remove(search)
             new_list.insert(i, replace)
     return new_list
