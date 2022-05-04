10fcf430e93ea95f6ec42bc8b995e3d7b3508515
nicobelen <nicolasbelen93@gmail.com>
2022-05-04 11:41:59 -0700

    sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print("Number: ", a_dictionary['Number'])
-    print("ids: ", a_dictionary['ids'])
-    print("language: ", a_dictionary['language'])
-    print("track: ", a_dictionary['track'])
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))
