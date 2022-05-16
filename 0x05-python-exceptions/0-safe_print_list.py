8c1f01778ff2d240357ae3ed71357fb9ba7356d1
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-16 13:24:44 -0300

    erase comments

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,4 @@ def safe_print_list(my_list=[], x=0):
             print()
         except IndexError:
                 print("")
-#        except:
-#                print("An unkown error occurred")
         return count
