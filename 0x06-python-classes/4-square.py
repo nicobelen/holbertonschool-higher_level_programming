4b7703bcdc98217e61cf1984de58c3aca945ab7e
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-17 20:00:31 -0300

    comments on 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""Creates class Square"""
+
+
 class Square:
+    """Initializes square instance"""
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+        """Retrieves size of square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets size of square"""
         if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,4 +22,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Returns are of square"""
         return (self.__size * self.__size)
