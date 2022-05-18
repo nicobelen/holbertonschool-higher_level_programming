5d3739f867d7aacf85fec7b68a5f096031d637e1
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-17 21:39:30 -0300

    102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Creates class Square"""
+
+
+class Square:
+    """Initializes square instance"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of square"""
+        if not isinstance(value, int) or not isinstance(value, float):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns are of square"""
+        return (self.__size * self.__size)
+    
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __lt__(self, other):
+        return self.area < other.area
+
+    def __le__(self, other):
+        return self.area <= other.area
+
+    def __ne__(self, other):
+        return self.area != other.area
+
+    def __gt__(self, other):
+        return self.area > other.area
+
+    def __ge__(self, other):
+        return self.area >= other.area
