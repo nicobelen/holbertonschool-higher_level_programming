0c194da7341c456ef9f38e31b0fe34ac24fe1550
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-18 23:48:47 -0300

    Ahora si el 6!!

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """Defines Rectangle"""
 
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """Initializes Rectangle instance"""
         self.height = height
