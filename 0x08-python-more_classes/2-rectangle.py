26bed16e5ef183a3679ddb4e1cc8ef3b2307b8ba
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-18 22:22:08 -0300

    correction 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Defines Rectangle"""
-    def __init__(self, width=0, height=0):
+    def init(self, width=0, height=0):
         """Initializes Rectangle instance"""
         self.__height = height
         self.__width = width
