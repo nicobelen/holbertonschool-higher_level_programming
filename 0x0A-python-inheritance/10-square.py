69f1853949bee1274467ac6556b21c2def72949b
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-30 18:52:49 -0300

    va a dar mal

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,36 +1,6 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """comment 1"""
-
-    def area(self):
-        """BaseGeometry"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """define area"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle"""
-
-    def __init__(self, width, height):
-        """define init"""
-        super().integer_validator('width', width)
-        super().integer_validator('height', height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Returns area of rectangle"""
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """Returns widht and height of Rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+"""Class import"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
