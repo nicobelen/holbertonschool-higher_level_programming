bd73369b251b2e43b2f99f01508de27ab8855f6e
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-31 18:20:20 -0300

    correction 2 on 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import json
-import sys
-
-
 def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, sys.stdout)
+        print(read_data)
