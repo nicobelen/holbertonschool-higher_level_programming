fb8dd250dd571cec16d2bcbbc169a21d4f3212de
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-31 19:15:27 -0300

    correction on 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-f = "add_item.json"
+filename = "add_item.json"
 
 try:
-    load = load_from_json_file(f)
+    load = load_from_json_file(filename)
 except FileNotFoundError:
     load = []
 
-save_to_json_file(load + argv[1:], f)
+save_to_json_file(load + argv[1:], filename)
