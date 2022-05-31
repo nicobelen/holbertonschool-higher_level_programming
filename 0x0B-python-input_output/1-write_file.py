0a0bdff1d12ac62ca32bc69d4fe9325951834050
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-31 18:35:54 -0300

    comments on 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""function that writes a string to a text file and returns 
+    the number of characters written"""
+
+
 def write_file(filename="", text=""):
+    """write the file"""
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
