0b73e00d3ae57109782c8b122ae365f01ccda1ea
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-31 18:30:56 -0300

    comments on 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Function that reads a text file and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """read the file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
