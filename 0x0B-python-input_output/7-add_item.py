36c0d6ca676a995aad689d5965ef73b4600645aa
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-01 20:33:57 -0300

    3rd correction on 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,5 +13,5 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 try:
     elm = load_from_json_file('add_item.json')
     save_to_json_file(elm + argv[1:], 'add_item.json')
-except FileNotFoundError:
+except Exception:
     save_to_json_file(argv[1:], 'add_item.json')
