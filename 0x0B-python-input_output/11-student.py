f349eca3c09da38e29654655a9132181ad0e44af
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-31 21:13:41 -0300

    pushing 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Retrieves a dictionary representation of a Student instance"""
+
+
+class Student:
+    """Defines class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """see comment up"""
+        if attrs is None:
+            return vars(self)
+        return ({key: value for key, value in vars(self).items()
+                 if key in attrs})
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the student instance"""
+        for key in json:
+            setattr(self, key, json[key])
