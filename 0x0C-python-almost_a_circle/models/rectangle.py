c0d53e30d11905b632f951c56f150d729ee4fbd6
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-07 11:02:02 -0300

    correction on return

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,4 +120,5 @@ class Rectangle(Base):
                     self.__y = value
 
     def to_dictionary(self):
-        return {'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height, 'width': self.width}
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                 'height': self.height, 'width': self.width}
