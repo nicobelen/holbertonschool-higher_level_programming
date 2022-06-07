1935e690c953fa00369170bc97222b2cd6642232
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-07 13:07:39 -0300

    correction on 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,6 +41,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or json_string == "":
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
