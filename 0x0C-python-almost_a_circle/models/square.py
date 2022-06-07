7f5d95d6ab2284b392dcac39049167bc265528d9
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-07 09:45:23 -0300

    pushing 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,23 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """define square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """call super class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Returns data of the square"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}")
+    
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
