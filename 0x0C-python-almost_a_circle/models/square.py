1a09146bcb39c45a7f107cbe928a02fa70822743
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-07 09:58:01 -0300

    pushing 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,28 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates values of the rectangle"""
+        count = 0
+        if args is not None and len(args) != 0:
+            for arg in args:
+                if count == 0:
+                    self.id = args[0]
+                if count == 1:
+                    self.size = args[1]
+                if count == 2:
+                    self.x = args[2]
+                if count == 3:
+                    self.y = args[3]
+                count += 1
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
