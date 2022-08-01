bdd185e9e15220d9f9b9b8a7ee743905cafb5d19
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-01 19:35:58 -0300

    Pushing 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+const arg = process.argv;
+const length = process.argv.length;
+let i = 2;
+let result = 0;
+let second = 0;
+if (!arg || length < 3) {
+  console.log('0');
+} else {
+  while (i < length && arg[i + 1]) {
+    if (arg[i] > arg[i + 1]) {
+      if (result < arg[i]) {
+        result = arg[i];
+      }
+    } else {
+      if (result < arg[i + 1]) {
+        result = arg[i + 1];
+      }
+    }
+    i++;
+  }
+  i = 2;
+  while (i < length) {
+    if (arg[i] > second && arg[i] < result) {
+      second = arg[i];
+    }
+    i++;
+  }
+}
+console.log(second);
