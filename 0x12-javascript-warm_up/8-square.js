aa8e94942b6ec7cd6c7c9333f16aebbc1130f91f
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-01 17:27:43 -0300

    Big push

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const str = 'X';
-let i = 0, j = 0;
+let i = 0;
+let j = 0;
 const arg = parseInt(process.argv[2], 10);
 if (!arg) {
   console.log('Missing size');
@@ -8,8 +9,8 @@ if (!arg) {
   while (i < arg) {
     j = 0;
     while (j < (arg - 1)) {
-        process.stdout.write(str);
-        j++;
+      process.stdout.write(str);
+      j++;
     }
     console.log(str);
     i++;
