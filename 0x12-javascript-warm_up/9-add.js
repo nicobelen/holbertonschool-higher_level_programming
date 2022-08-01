6f06b5b9e749610050a25f24ed9d403b654652fe
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-01 17:56:49 -0300

    Correction on 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-let result;
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
 function add (a, b) {
   return (a + b);
 }
-a = parseInt(process.argv[2], 10);
-b = parseInt(process.argv[3], 10);
-
 console.log(add(a, b));
