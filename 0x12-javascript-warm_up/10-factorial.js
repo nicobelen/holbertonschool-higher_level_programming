96b0b30205b42b9f684e53a95d99c1675dc165f2
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-01 18:50:02 -0300

    Pushing 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const num = parseInt(process.argv[2], 10);
+function factorialize (num) {
+  if (num === 0 || !num) {
+    return 1;
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+console.log(factorialize(num));
